6e7704697fdf2d4ee494dc3786d13b53e712dda6
Silas Villon <silas.villon@corp.globo.com>
2012-06-15 19:40:38 -0300

    comecando models novo

--- home/models.py
+++ home/models.py
@@ -2,27 +2,19 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-#from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 
 class Produto(models.Model):
+    nome = models.CharField(max_length=100)
     preco = models.CommaSeparatedIntegerField(max_length=10, verbose_name="preço")
-   #qtd_total = models.IntegerField()
-   #qtd_em_estoque = models.IntegerField(blank=True)
+    qtd_total = models.IntegerField()
+    qtd_em_estoque = models.IntegerField(blank=True)
 
-   #def save(self, *args, **kwargs):
-   #    self.qtd_em_estoque = self.qtd_total
-   #    super(Produto, self).save(*args, **kwargs)
-
-
-class Console(Produto):
-    nome = models.CharField(max_length=10)
+    def save(self, *args, **kwargs):
+        self.qtd_em_estoque = self.qtd_total
+        super(Produto, self).save(*args, **kwargs)
 
 
-class Game(Produto):
-    titulo = models.CharField(max_length=10, verbose_name="título")
-    console = models.ForeignKey(Console)
-
-
-#class Cliente(User):
-#    cpf = models.CharField(max_length=14, unique=True)
+class Cliente(User):
+    cpf = models.CharField(max_length=14, unique=True)
