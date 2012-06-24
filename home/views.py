be9beb809c2fe4ea39245b944edea380b1a59c96
Silas Villon <silas.villon@corp.globo.com>
2012-06-24 15:47:29 -0300

    desfazendo o trabalho de uma manha inteira

--- home/views.py
+++ home/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from home.models import Produto
+from django.db.models import CommaSeparatedIntegerField
 
 
 def home(request):
@@ -30,11 +31,25 @@ def registrar(request):
 
 @login_required
 def comprar(request):
-    query = request.GET['ids']
-    lista_ids = query.split(',')
+    prod_id = request.GET['prod_id']
+    #lista_ids = query.split(',')
+    if not 'prod_ids' in request.session.keys():
+        request.session['prod_ids'] = [prod_id]
+    else:
+        if prod_id not in request.session['prod_ids']:
+            request.session['prod_ids'] += [prod_id]
+
     #insira aqui uma list comprehension boladona que retorna os produtos!
-    produtos = [ Produto.objects.get(id=prod_id) for prod_id in lista_ids ]
+    produtos = [ Produto.objects.get(id=prod_id) for prod_id in request.session['prod_ids']]
+    import ipdb; ipdb.set_trace()
+    preco_total = 0
+    for produto in produtos:
+        sp = produto.preco.split(',')
+        preco = 100 * int(sp[0]) + int(sp[1])
+        preco_total += preco
+    str_tot = unicode(preco_total)
+    str_tot2 = str_tot[:-2] + ',' + str_tot[-2:]
 
     return render_to_response('comprar.html',
-                              {'produtos': produtos},
+                              {'produtos': produtos, 'tot': str_tot2},
                              context_instance=RequestContext(request))
